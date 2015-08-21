59521f1aa7f4b412ce086467654d5afe2991dd8f
madisoncarpenter <madisoncarpenter@users.noreply.github.com>
2015-08-21 04:49:16 -0700

    First

---
+++ testing.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+
+ * Plugin Name: Resume Check API
+
+ * Description: TBD
+
+ * Version: 1.0
+
+ * Author: Carp
+
+ * License: GPL2
+
+ */
+
+
+
+add_action('admin_menu', 'carp_admin_menu');
+
+add_action('gform_entry_created','carp_trigger',10,2);
+
+dA
+
+?>
